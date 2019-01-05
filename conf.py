4127125337862d3176ecd951310128947e2cf6cf
Mike Jarrett <msjarrett@gmail.com>
2019-01-05 14:02:07 -0800

    Nikola auto commit.
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -150,8 +150,8 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-#THEME = "bootblog4"
-THEME = "bootyper"
+THEME = "bootstrap3"
+#THEME = "bootyper"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
